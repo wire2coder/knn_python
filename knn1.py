e449ca68dc444cbbd29e3d0c00685b822c3d42e3
Andy T <met2dev2user@met2dev2.turner.com>
2020-03-09 08:27:53 -0400

    look at the paper note, to review

--- knn1.py
+++ knn1.py
@@ -8,45 +8,72 @@
     https://towardsdatascience.com/machine-learning-basics-with-the-k-nearest-neighbors-algorithm-6a6e71d01761
  """
 
+
 from collections import Counter
 import math
+import math
 
-
-def knn(data, query, k, distancce_fn, choice_fn):
-    # making a new 'list'
-    neighbor_distance_and_indices = []
-
-    # 3. for each 'example' in the data
+def knn(data, query, k, distance_fn, choice_fn):
+    neighbor_distances_and_indices = []
+    
+    # 3. For each example in the data
     for index, example in enumerate(data):
-        # 3.1 calculate the distance between the '' and ''
+
+        # debug
+        print("query", query)
+        print("example[:-1]", example[:-1])
+
+        # 3.1 Calculate the distance between the query example and the current
+        # example from the data.
         distance = distance_fn(example[:-1], query)
 
-        # 3.2 and the distance to the 'list'
-        neighbor_distance_and_indices.append( (distance, index) )
+        # debug
+        print("distance", distance)
+        
+        # 3.2 Add the distance and the index of the example to an ordered collection
+        neighbor_distances_and_indices.append((distance, index))
+    
+    # 4. Sort the ordered collection of distances and indices from
+    # smallest to largest (in ascending order) by the distances
+    sorted_neighbor_distances_and_indices = sorted(neighbor_distances_and_indices)
+    
+    # 5. Pick the first K entries from the sorted collection
+    k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
+    
+    # 6. Get the labels of the selected K entries
+    k_nearest_labels = [data[i][1] for distance, i in k_nearest_distances_and_indices]
 
-        # 4 /sort/ the 'list' from smallest to largest
-        sorted_neighbor_distances_and_indices = sorted(neighbor_distance_and_indices)
+    # 7. If regression (choice_fn = mean), return the average of the K labels
+    # 8. If classification (choice_fn = mode), return the mode of the K labels
+    return k_nearest_distances_and_indices , choice_fn(k_nearest_labels)
 
-        # 5 pick the first 'K'
-        k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
+def mean(labels):
+    return sum(labels) / len(labels)
 
-        # 6 get the 'lable' of the 'K' << here, what is this line doing????
-        k_nearest_lables = [ data[i][1] for distance, i in k_nearest_distances_and_indices ]
+def mode(labels):
+    return Counter(labels).most_common(1)[0][0]
 
-        # 7. If regression (choice_fn = mean), return the average of the K labels
-        # 8. If classification (choice_fn = mode), return the mode of the K labels
-        return k_nearest_distances_and_indices, choice_fn(k_nearest_lables)
+def euclidean_distance(point1, point2):
 
+    # debug
+    # len(point1) >> 1
+    # range(len(point1)) >> 0
+
+    sum_squared_distance = 0
+    for i in range(len(point1)):
+        sum_squared_distance += math.pow(point1[i] - point2[i], 2)
+
+    # debug
+    print("sum_squared_distance", sum_squared_distance)    
+    return math.sqrt(sum_squared_distance)
 
 def main():
-
     '''
     # Regression Data
     # 
     # Column 0: height (inches)
     # Column 1: weight (pounds)
     '''
-
     reg_data = [
        [65.75, 112.99],
        [71.52, 136.49],
@@ -59,14 +86,44 @@ def main():
        [67.90, 112.37],
        [66.49, 127.45],
     ]
-
+    
     # Question:
     # Given the data we have, what's the best-guess at someone's weight if they are 60 inches tall?
     reg_query = [60]
-    reg_k_neareset_neighbors, reg_prediction = knn( reg_data, reg_query, k=3, distancce_fn=euclidean_distance, choice_fn=mean )
+    reg_k_nearest_neighbors, reg_prediction = knn(
+        reg_data, reg_query, k=3, distance_fn=euclidean_distance, choice_fn=mean
+    )
+    
+    '''
+    # Classification Data
+    # 
+    # Column 0: age
+    # Column 1: likes pineapple
+    '''
+    clf_data = [
+       [22, 1],
+       [23, 1],
+       [21, 1],
+       [18, 1],
+       [19, 1],
+       [25, 0],
+       [27, 0],
+       [29, 0],
+       [31, 0],
+       [45, 0],
+    ]
+    # Question:
+    # Given the data we have, does a 33 year old like pineapples on their pizza?
+    clf_query = [33]
+    clf_k_nearest_neighbors, clf_prediction = knn(
+        clf_data, clf_query, k=3, distance_fn=euclidean_distance, choice_fn=mode
+    )
 
-    here
+    print(">>><<<<><><<>>>>")
+    print("clf_k_nearest_neighbors", clf_k_nearest_neighbors)
+    print("clf_prediction", clf_prediction)
+    print(">>><<<<><><<>>>>")
 
-
+# run the 'main' function
 if __name__ == '__main__':
     main()
